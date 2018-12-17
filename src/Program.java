13296b86a6294c92570c76de364136fdb6057e99
simurashka <simurashka@gmail.com>
2018-12-17 23:03:24 +0200

    basic 1 & 2

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,12 @@
+import com.w3resource.javaexercises.basic.PrintHello;
+import com.w3resource.javaexercises.basic.SumOfTwoNumbers;
+
+
 public class Program {
 
     public static void main (String[] args) {
 
+        new SumOfTwoNumbers().printSum(56, 3);
     }
-    
+
 }
---
+++ src/com/w3resource/javaexercises/basic/PrintHello.java
@@ -0,0 +1,10 @@
+package com.w3resource.javaexercises.basic;
+
+/**
+ * 1. Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+ */
+public class PrintHello {
+    public void print(String name) {
+        System.out.println("Hello,\n" + name + "!");
+    }
+}
