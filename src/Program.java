cb36907a29969990d9e8cf38b07440d7e34bda6b
simurashka <simurashka@gmail.com>
2019-01-06 20:23:56 +0200

    sumOfTwoNumbers

--- src/Program.java
+++ src/Program.java
@@ -5,8 +5,12 @@ import com.w3resource.javaexercises.basic.SumOfTwoNumbers;
 public class Program {
 
     public static void main (String[] args) {
-
-        new SumOfTwoNumbers().printSum(56, 3);
+        var sum = new SumOfTwoNumbers();
+        sum.printSum(56, 3);
+        sum.printSum(56.12, 4.89);
+        sum.printSum(1.0, 2.0);
     }
+
+
 
 }
