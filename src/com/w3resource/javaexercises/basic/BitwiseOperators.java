eeb1c8a06ec37e58a489dd2afc71e06fb3c5a8db
simurashka <simurashka@gmail.com>
2019-01-16 23:03:45 +0200

    Bitwise padZeroes(), or()

--- src/com/w3resource/javaexercises/basic/BitwiseOperators.java
+++ src/com/w3resource/javaexercises/basic/BitwiseOperators.java
@@ -3,10 +3,18 @@ package com.w3resource.javaexercises.basic;
 public class BitwiseOperators {
 
     public String binaryValue (int a) {
-        return String.format("%08d", Integer.parseInt(Integer.toBinaryString(a)));
+        return Integer.toBinaryString(a);
+    }
+
+    public String padZeroes (int a, int digits) {
+        return String.format("%0" + digits + "d", Integer.parseInt(binaryValue(a)));
     }
 
     public int and (int a, int b) {
         return a & b;
+    }
+
+    public int or (int a, int b) {
+        return a | b;
     }
 }
--- test/com/w3resource/javaexercises/basic/BitwiseOperatorsTest.java
+++ test/com/w3resource/javaexercises/basic/BitwiseOperatorsTest.java
@@ -11,9 +11,16 @@ class BitwiseOperatorsTest {
 
     @Test
     void binaryValue() {
-        assertEquals("00000011", bit.binaryValue(a));
-        assertEquals("00000100", bit.binaryValue(b));
-        assertEquals("00001110", bit.binaryValue(c));
+        assertEquals("11", bit.binaryValue(a));
+        assertEquals("100", bit.binaryValue(b));
+        assertEquals("1110", bit.binaryValue(c));
+    }
+
+    @Test
+    void padZeroes() {
+        assertEquals("0011", bit.padZeroes(a,4));
+        assertEquals("100", bit.padZeroes(b,2));
+        assertEquals("00001110", bit.padZeroes(c,8));
     }
 
     @Test
@@ -21,5 +28,12 @@ class BitwiseOperatorsTest {
         assertEquals(0, bit.and(a, b));
         assertEquals(4, bit.and(b, c));
         assertEquals(2, bit.and(a, c));
+    }
+
+    @Test
+    void or() {
+        assertEquals(7, bit.or(a, b));
+        assertEquals(14, bit.or(b, c));
+        assertEquals(15, bit.or(a, c));
     }
 }
